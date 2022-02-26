2dfbb800d5f5120dd0dab5255979075ff0347f12
Airz7 <100176111+Airz7@users.noreply.github.com>
2022-02-26 16:25:57 -0500

    Update main.js

--- script/main.js
+++ script/main.js
@@ -223,7 +223,7 @@ const animationTimeline = () => {
     .from(".hat", 0.5, {
       x: 700,
       y: 400,
-      rotation: -180,
+      rotation: 180,
       opacity: 0
     })
     .staggerFrom(
